d9f0a89c59f233e421965de9e3f0ccdf15a8d423
Derek Nordgren <derek.nordgren@hudl.com>
2021-08-22 23:37:40 +0100

    fix: hack out widgetbot due to failing prod build
    window and document aren't available in node.js server-side renders.
    tried to patch it but unsuccessfully - used by my code and the
    @widgetbot package internally. fuck gatsby.

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import Layout from '@/components/Layout'
-import WidgetBot from '@widgetbot/react-embed'
+// import WidgetBot from '@widgetbot/react-embed'
 
 const ChatPage = () => {
-  const browserHeight = document.body.offsetHeight
-  const browserWidth = document.body.offsetWidth
+  // const browserHeight = document.body.offsetHeight
+  // const browserWidth = document.body.offsetWidth
 
-  const discordWidgetHeight = Math.max(browserHeight - 200 - 100, 500) // 200 = height of header, footer + padding
-  const discordWidgetWidth = Math.max(browserWidth - 150, 500) // padding
+  // const discordWidgetHeight = Math.max(browserHeight - 200 - 100, 500) // 200 = height of header, footer + padding
+  // const discordWidgetWidth = Math.max(browserWidth - 150, 500) // padding
 
   return (
     <Layout>
       {// https://docs.widgetbot.io/embed/react-embed/props/
       }
-      <WidgetBot
+      {/* <WidgetBot
         server="870404843582406696" // Der Bruden
         channel="870404844681322549" // #bruden
-        height={discordWidgetHeight}
-        width={discordWidgetWidth}
-      />
+        height={750}
+        width={1250}
+      /> */}
     </Layout>
   )
 }
